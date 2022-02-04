44e35d64a5f07b6aff5f41199db1d22606dbd32c
Missb123# <chinelsy@yahoo.co.uk>
2022-02-04 15:46:14 +0100

    initial commit

---
+++ PhoneBookApp/Models/Contact.cs
@@ -0,0 +1,13 @@
+﻿namespace PhoneBookApp.Models
+{
+    public class Contact
+    {
+        public Contact(string name, int number)
+        {
+            Name = name;
+            Number = number;
+        }
+        public string Name { get; set; }
+        public int Number { get; set; }
+    }
+}
---
+++ PhoneBookApp/Models/PhoneBook.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PhoneBookApp.Models
+{
+    public class PhoneBook
+    {
+        private List<Contact> _contacts { get; set; } = new List<Contact>();
+        private static void DisplayContactDetails(Contact contact)
+        {
+            Console.WriteLine($"Contact:{contact.Name}, {contact.Number}");
+
+        }
+
+        public void AddContact(Contact contact)
+        {
+            _contacts.Add(contact); 
+        }
+
+        public void DisplayContact(int number)
+        {
+            var contact = _contacts.FirstOrDefault(c => c.Number == number);
+            if (contact == null)
+            {
+                Console.WriteLine("Contact not found");
+            }
+            else
+            {
+                DisplayContactDetails(contact);
+            }
+        }
+
+        public void DisplayContacts()
+        {
+            foreach (var contact in _contacts)
+            {
+                DisplayContactDetails(contact);
+            }
+        }
+
+        //This method searches for a given contact
+        //named: bill
+        //searchphrase: ll
+        public void DisplayMatchingContacts(string searchPhrase)
+        {
+            var matchingContacts = _contacts.Where(c => c.Name.Contains(searchPhrase)).ToList();
+            foreach (var contact in matchingContacts)
+            {
+                DisplayContactDetails(contact);
+            }
+        }
+    }
+}
